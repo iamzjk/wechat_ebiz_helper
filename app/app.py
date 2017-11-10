6b415de644de5071f9af9a2f60afec987a00d8ad
Jingkun <iamzjk@hotmail.com>
2017-11-10 10:09:43 -0500

    login and order api

--- app/app.py
+++ app/app.py
@@ -6,23 +6,38 @@
     https://stackoverflow.com/questions/9692962/flask-sqlalchemy-import-context-issue/9695045#9695045
 '''
 
-from flask import Flask, render_template, redirect, url_for, jsonify, request
+import datetime
+from functools import wraps
+
+from flask import Flask, render_template, redirect, url_for, jsonify, request, make_response
 from flask_mysqldb import MySQL
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms import StringField
 from wtforms.validators import DataRequired
 from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+import jwt
+import uuid
 
 from config import config
 from tracking import Tracking
+from models import db, Order, User
 
 app = Flask(__name__)
 Bootstrap(app)
 app.config.update(config)
+
 mysql = MySQL(app)
+db.init_app(app)
+
 cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
+
+###
+#   Forms
+###
 
 class ShowMyOrderForm(FlaskForm):
     client = StringField('收件人', validators=[DataRequired()])
@@ -54,7 +69,8 @@ def orders(client, phone):
             price,
             quantity,
             DATE(created_time) AS created_time
-        FROM usatocn2013.orders;
+        FROM usatocn2013.orders
+        ORDER BY created_time DESC;
         '''
     else:
         query = '''
@@ -68,7 +84,8 @@ def orders(client, phone):
             DATE(created_time) AS created_time
         FROM usatocn2013.orders
         WHERE client = '{client}'
-        AND phone = '{phone}';
+        AND phone = '{phone}'
+        ORDER BY created_time DESC;
         '''.format(client=client, phone=phone)
 
     cur.execute(query)
@@ -92,70 +109,239 @@ def tracking_status(tracking_number, carrier):
 # API
 ###
 
-
-@app.route('/api/orders/<client>/<phone>')
-def get_my_orders(client, phone):
-    cur = mysql.connection.cursor()
-
-    if (client == 'lucaslinus') and (phone == 'linuslucas'):
-        query = '''
-        SELECT
-            client,
-            tracking,
-            carrier,
-            product,
-            price,
-            quantity,
-            DATE(created_time) AS created_time
-        FROM usatocn2013.orders;
-        '''
-    else:
-        query = '''
-        SELECT
-            client,
-            tracking,
-            carrier,
-            product,
-            price,
-            quantity,
-            DATE(created_time) AS created_time
-        FROM usatocn2013.orders
-        WHERE client = '{client}'
-        AND phone = '{phone}';
-        '''.format(client=client, phone=phone)
-
-    cur.execute(query)
-    orders = cur.fetchall()
-
-    if not orders:
-        return jsonify({'data': 'no order found'})
-    else:
-        return jsonify({'data': orders})
+###
+#   Authentication API
+###
 
 
-@app.route('/api/tracking/<tracking_number>/<carrier>')
-def get_tracking_status(tracking_number, carrier):
-    tracking_obj = Tracking.get_tracking_object(tracking_number, carrier)
-    statuses = tracking_obj.track()
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = request.headers.get('x-access-token')
 
-    return jsonify({'data': statuses})
+        if not token:
+            return jsonify({'message': 'Token is missing!'}), 401
+
+        try:
+            data = jwt.decode(token, app.config['SECRET_KEY'])
+            current_user = User.query.filter_by(username=data['username']).first()
+        except:
+            return jsonify({'message': 'Token is invalid!'}), 401
+
+        return f(current_user, *args, **kwargs)
+
+    return decorated
 
 
-@app.route('/api/orders/', methods=['POST'])
-def add_one_new_order():
-    new_order = request.json['order']
-    _insert_one_order(new_order)
 
-    return jsonify({'data': {'order': new_order, 'status': 'inserted'}})
+@app.route('/api/login')
+def login():
+    auth = request.authorization
+
+    if not auth or not auth.username or not auth.password:
+        return make_response('Could not verify', 401, {'WWW-Authenticate': 'Basic realm="Login required!"'})
+
+    user = User.query.filter_by(username=auth.username).first()
+
+    if not user:
+        return make_response('Could not verify', 401, {'WWW-Authenticate': 'Basic realm="Login required!"'})
+
+    if check_password_hash(user.password, auth.password):
+        token = jwt.encode({
+            'username': user.username,
+            'exp': datetime.datetime.utcnow() + datetime.timedelta(hours=24)},
+            app.config['SECRET_KEY']
+        )
+
+        return jsonify({'token': token.decode('UTF-8')})
+
+    return make_response('Could not verify', 401, {'WWW-Authenticate': 'Basic realm="Login required!"'})
 
 
-def _insert_one_order(order):
-    query = '''
-    INSERT INTO usatocn2013.orders(client, phone, product, price, cost, quantity, tracking, carrier)
-    VALUES("{client}", "{phone}", "{product}", {price}, {cost}, {quantity}, "{tracking}", "{carrier}")
-    '''.format(**order)
+@app.route('/api/user', methods=['GET'])
+@token_required
+def get_all_users(current_user):
 
-    con = mysql.connection
-    cur = con.cursor()
-    cur.execute(query)
-    con.commit()
+    if not current_user.admin:
+        return jsonify({'message': 'Cannot perform that function!'})
+
+    users = User.query.all()
+
+    output = []
+
+    for user in users:
+        user_data = {
+            'username': user.username,
+            'password': user.password,
+            'admin': user.admin,
+        }
+        output.append(user_data)
+
+    return jsonify({'users': output})
+
+
+@app.route('/api/user/<username>', methods=['GET'])
+@token_required
+def get_one_user(current_user, username):
+
+    if not current_user.admin:
+        return jsonify({'message': 'Cannot perform that function!'})
+
+    user = User.query.filter_by(username=username).first()
+
+    if not user:
+        return jsonify({'message': 'No user found!'})
+
+    user_data = {
+        'username': user.username,
+        'password': user.password,
+        'admin': user.admin,
+    }
+
+    return jsonify({'user': user_data})
+
+
+@app.route('/api/user', methods=['POST'])
+@token_required
+def create_user(current_user):
+    if not current_user.admin:
+        return jsonify({'message' : 'Cannot perform that function!'})
+
+    data = request.get_json()
+
+    username = data['username']
+    if User.query.filter_by(username=username).first():
+        return jsonify({'message': 'Username already exists.'})
+
+    hashed_password = generate_password_hash(data['password'], method='sha256')
+
+    new_user = User(
+        username=username, password=hashed_password, admin=False
+    )
+    db.session.add(new_user)
+    db.session.commit()
+
+    return jsonify({'message': 'New user created!'})
+
+
+@app.route('/api/user/<username>', methods=['PUT'])
+@token_required
+def promote_user(current_user, username):
+    if not current_user.admin:
+        return jsonify({'message': 'Cannot perform that function!'})
+
+    user = User.query.filter_by(username=username).first()
+
+    if not user:
+        return jsonify({'message': 'No user found!'})
+
+    user.admin = True
+    db.session.commit()
+
+    return jsonify({'message': 'The user has been promoted!'})
+
+
+@app.route('/api/user/<username>', methods=['DELETE'])
+@token_required
+def delete_user(current_user, username):
+    if not current_user.admin:
+        return jsonify({'message': 'Cannot perform that function!'})
+
+    user = User.query.filter_by(username=username).first()
+
+    if not user:
+        return jsonify({'message': 'No user found!'})
+
+    db.session.delete(user)
+    db.session.commit()
+
+    return jsonify({'message': 'The user has been deleted!'})
+
+
+@app.route('/api/order', methods=['GET'])
+@token_required
+def get_all_orders(current_user):
+    if not current_user.admin:
+        return jsonify({'message': 'Cannot perform that function!'})
+
+    orders = Order.query.order_by(Order.created_time.desc()).all()
+
+    output = []
+    for order in orders:
+        new = {
+            'order_id': str(order.order_id),
+            'client': order.client,
+            'phone': order.phone,
+            'product': order.product,
+            'price': str(round(order.price, 2)),
+            'cost': str(round(order.cost, 2)),
+            'quantity': str(order.quantity),
+            'tracking': order.tracking,
+            'carrier': order.carrier,
+            'created_time': order.created_time,
+        }
+        output.append(new)
+
+    return jsonify({'orders': output})
+
+
+@app.route('/api/order', methods=['POST'])
+@token_required
+def create_order(current_user):
+    if not current_user.admin:
+        return jsonify({'message': 'Cannot perform that function!'})
+
+    data = request.get_json()
+
+    new_order = Order(
+        client=data['client'],
+        phone=data['phone'],
+        product=data['product'],
+        price=data['price'],
+        cost=data.get('cost'),
+        quantity=data['quantity'],
+        tracking=data.get('tracking', ''),
+        carrier=data.get('carrier', ''),
+    )
+    db.session.add(new_order)
+    db.session.commit()
+
+    return jsonify({'message': "Order created!"})
+
+
+@app.route('/api/order/<order_id>', methods=['PUT'])
+@token_required
+def update_order(current_user, order_id):
+    if not current_user.admin:
+        return jsonify({'message': 'Cannot perform that function!'})
+
+    order = Order.query.filter_by(order_id=order_id).first()
+
+    if not order:
+        return jsonify({'message': 'No order found!'})
+
+    data = request.get_json()
+
+    for key, value in data.items():
+        setattr(order, key, value)
+
+    db.session.commit()
+
+    return jsonify({'message': 'Order record has been updated!'})
+
+
+@app.route('/api/order/<order_id>', methods=['DELETE'])
+@token_required
+def delete_order(current_user, order_id):
+    if not current_user.admin:
+        return jsonify({'message': 'Cannot perform that function!'})
+
+    order = Order.query.filter_by(order_id=order_id).first()
+
+    if not order:
+        return jsonify({'message': 'No order found!'})
+
+    db.session.delete(order)
+    db.session.commit()
+
+    return jsonify({'message': 'Order record deleted!'})
---
+++ app/models.py
@@ -0,0 +1,47 @@
+# coding: utf-8
+from sqlalchemy import Column, DateTime, Float, Integer, String, text
+from flask_sqlalchemy import SQLAlchemy
+db = SQLAlchemy()
+
+
+class Order(db.Model):
+    __tablename__ = 'orders'
+
+    order_id = Column(Integer, primary_key=True)
+    client = Column(String(25, 'utf8_unicode_ci'), nullable=False)
+    phone = Column(String(15, 'utf8_unicode_ci'), nullable=False)
+    product = Column(String(50, 'utf8_unicode_ci'), nullable=False)
+    price = Column(Float(10, True), nullable=False)
+    cost = Column(Float(10, True), nullable=False)
+    quantity = Column(Integer, nullable=False)
+    tax_pct = Column(Float(10, True), nullable=False, server_default=text("'0.00'"))
+    tracking = Column(String(50, 'utf8_unicode_ci'), server_default=text("''"))
+    carrier = Column(String(15, 'utf8_unicode_ci'), server_default=text("''"))
+    created_time = Column(DateTime, server_default=text("CURRENT_TIMESTAMP"))
+    updated_time = Column(DateTime)
+
+
+class OrdersTest(db.Model):
+    __tablename__ = 'orders_test'
+
+    order_id = Column(Integer, primary_key=True)
+    client = Column(String(25, 'utf8_unicode_ci'), nullable=False)
+    phone = Column(String(15, 'utf8_unicode_ci'), nullable=False)
+    product = Column(String(50, 'utf8_unicode_ci'), nullable=False)
+    price = Column(Float(10, True), nullable=False)
+    cost = Column(Float(10, True), nullable=False)
+    quantity = Column(Integer, nullable=False)
+    tax_pct = Column(Float(10, True), nullable=False, server_default=text("'0.00'"))
+    tracking = Column(String(50, 'utf8_unicode_ci'), server_default=text("''"))
+    carrier = Column(String(15, 'utf8_unicode_ci'), server_default=text("''"))
+    created_time = Column(DateTime, server_default=text("CURRENT_TIMESTAMP"))
+    updated_time = Column(DateTime)
+
+
+class User(db.Model):
+    __tablename__ = 'user'
+
+    username = Column(String(50), primary_key=True)
+    password = Column(String(80))
+    admin = Column(Integer)
+    comment = Column(String(50))
